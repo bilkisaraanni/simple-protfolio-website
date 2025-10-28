bc84569c77ba186970f09fc1e0cce5f07ce778fb
bilkisaraanni <bilkisara28@mail.com>
2025-10-28 17:04:58 +0600

    Updated project files

--- script.js
+++ script.js
@@ -11,3 +11,73 @@ menu.addEventListener('click', () => {
         show = false;
      }
 });
+
+// 5th part countdown
+
+let count = document.querySelector('.count');
+let countone = document.querySelector('.countone');
+let counttwo = document.querySelector('.counttwo');
+let countthree = document.querySelector('.countthree');
+
+let counter = 0;
+let counterOne = 0;
+let counterTwo = 0;
+let counterThree = 0;
+
+let intervel = setInterval(() => {
+    count.textContent = counter;
+    counter++
+    if( counter > 2){
+     
+       clearInterval(intervel)
+    }
+},1000);
+
+let intervelOne = setInterval(()=>{
+   countone.textContent = counterOne;
+   counterOne++
+   if( counterOne > 10){
+      clearInterval(intervelOne)
+   }
+},400);
+
+let intervelTwo = setInterval(()=>{
+   counttwo.textContent = counterTwo;
+   counterTwo++
+   if( counterTwo > 20){
+      clearInterval(intervelTwo)
+   }
+},200);
+
+
+let intervelThree = setInterval(()=>{
+   countthree.textContent =counterThree;
+   counterThree++
+   if( counterThree > 50){
+      clearInterval(intervelThree)
+   }
+},50);
+
+
+
+
+
+// footer part 
+
+let name = document.querySelector('.name');
+let lastname = document.querySelector('.lastname');
+let email = document.querySelector('.email');
+let phon = document.querySelector('.phon');
+
+
+let sendbtn = document.querySelector('.sendbtn');
+let error = document.querySelector('.error');
+
+sendbtn.addEventListener('click',()=>{
+   let inputName = name.value;
+   if(inputName ==""){
+     error.textContent = 'Filp up the from'
+   }
+   
+})
+
